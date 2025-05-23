79d71730b36e4dff1f0969fdad702066fb67cbac
devvratmiglani <devvratmiglani@gmail.com>
2025-05-24 00:49:30 +0530

    Added CHanges to windows_shortcut.py for linux compatibility

--- skribbl/windows_shortcut.py
+++ skribbl/windows_shortcut.py
@@ -1,11 +1,14 @@
 import os
 
-if os.name == 'nt':
-    from win32com.client import Dispatch
-    def create_shortcut(name, target, arguments="", icon=None):
-        desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), "OneDrive\Desktop")
+def create_shortcut(name, target, arguments="", icon=None):
+    if os.name != 'nt':
+        print("Shortcut creation is not supported on this OS.")
+        return
+    try:
+        from win32com.client import Dispatch
+        desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), "OneDrive","Desktop")
         path = os.path.join(desktop, f"{name}.lnk")
-
+        
         shell = Dispatch('WScript.Shell')
         shortcut = shell.CreateShortCut(path)
         shortcut.Targetpath = target
@@ -14,10 +17,9 @@ if os.name == 'nt':
         if icon:
             shortcut.IconLocation = icon
         shortcut.save()
-else:
-    def create_shortcut(name, target, arguments="", icon=None):
-        peint("Not Implemented for this Operating system")
-    
+    except Exception as e:
+        print(f"❌ Shortcut creation failed with exception {e}")
+
 
 # Shortcut for: skribbl-public
 # create_shortcut("Skribble - Public Room", "skribbl-public.exe")
